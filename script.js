e4ff5aed2f0d60a0be8adf417a123b33a73a97f6
Mozharov Sergey <mozharov-ss@yandex.ru>
2022-07-09 14:52:11 +0300

    feat: добавить интерактивность лайков и адаптивность блока profile

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let popup = document.querySelector(".popup");
 let editButton = document.querySelector(".edit-button");
 let closeButton = document.querySelector(".popup__close");
 let submitProfileButton = document.querySelector(".submit-button");
+let cards = document.querySelector(".cards");
 
 function editProfileOpen() {
   popup.classList.add("popup_opened");
@@ -16,14 +17,22 @@ function formSubmitHandler(evt) {
   let newUserName = popup.querySelector(".popup__field_name").value;
   let newUserFunction = popup.querySelector(".popup__field_function").value;
 
-  let profileName = document.querySelector('.profile__name');
-  let profileFunction = document.querySelector('.profile__function');
+  let profileName = document.querySelector(".profile__name");
+  let profileFunction = document.querySelector(".profile__function");
 
   profileName.innerText = newUserName;
   profileFunction.innerText = newUserFunction;
-  editProfileClose()
+  editProfileClose();
+}
+
+function likeToggler(event) {
+  target = event.target;
+  if (target.classList.contains("like-button__svg")) {
+    target.classList.toggle("like-button__svg_active");
+  }
 }
 
 editButton.addEventListener("click", editProfileOpen);
 closeButton.addEventListener("click", editProfileClose);
 submitProfileButton.addEventListener("click", formSubmitHandler);
+cards.addEventListener("click", likeToggler);

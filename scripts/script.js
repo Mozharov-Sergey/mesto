8a779bbee3b7e55b6e0471d1ea416999000267a9
Mozharov Sergey <mozharov-ss@yandex.ru>
2022-07-11 22:46:20 +0300

    fix: ревью 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,16 @@
+// POPUP
 let popup = document.querySelector(".popup");
-let editButton = document.querySelector(".edit-button");
 let closeButton = document.querySelector(".popup__close");
-let submitProfileButton = document.querySelector(".submit-button");
-let cards = document.querySelector(".cards");
+let popupForm = document.querySelector(".popup__form");
+// Значения newUserName и newUserFunction вычисляются
+// в момент вызова submitPopupFormHandler
+let newUserName;
+let newUserFunction;
+
+// PROFILE
+let editButton = document.querySelector(".profile__edit-button");
+let profileName = document.querySelector(".profile__name");
+let profileFunction = document.querySelector(".profile__profession");
 
 function editProfileOpen() {
   popup.classList.add("popup_opened");
@@ -12,27 +20,30 @@ function editProfileClose() {
   popup.classList.remove("popup_opened");
 }
 
-function formSubmitHandler(evt) {
+function submitPopupFormHandler(evt) {
   evt.preventDefault();
-  let newUserName = popup.querySelector(".popup__field_name").value;
-  let newUserFunction = popup.querySelector(".popup__field_function").value;
 
-  let profileName = document.querySelector(".profile__name");
-  let profileFunction = document.querySelector(".profile__function");
+  newUserName = popup.querySelector(".popup__field_value_name").value;
+  newUserFunction = popup.querySelector(".popup__field_value_profession").value;
 
-  profileName.innerText = newUserName;
-  profileFunction.innerText = newUserFunction;
+  profileName.textContent = newUserName;
+  profileFunction.textContent = newUserFunction;
   editProfileClose();
-}
-
-function likeToggler(event) {
-  target = event.target;
-  if (target.classList.contains("like-button")) {
-    target.classList.toggle("like-button_active");
-  }
 }
 
 editButton.addEventListener("click", editProfileOpen);
 closeButton.addEventListener("click", editProfileClose);
-submitProfileButton.addEventListener("click", formSubmitHandler);
+popupForm.addEventListener("submit", submitPopupFormHandler);
+
+// Эти переменная, слушатель и его хендлер likeToggler  будут использованы в следующем спринте
+
+let cards = document.querySelector(".cards");
+
+function likeToggler(event) {
+  target = event.target;
+  if (target.classList.contains("cards__like-button")) {
+    target.classList.toggle("cards__like-button_active");
+  }
+}
+
 cards.addEventListener("click", likeToggler);

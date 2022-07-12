2ce13bf28639e00923010bea1c2c22cc1334d31d
Mozharov Sergey <mozharov-ss@yandex.ru>
2022-07-12 15:13:31 +0300

    fix: Ревью 2  Исправитьломающуюся при переполнении верстку и сделать popup всегда по центру монитора

--- scripts/script.js
+++ scripts/script.js
@@ -2,18 +2,18 @@
 let popup = document.querySelector(".popup");
 let closeButton = document.querySelector(".popup__close");
 let popupForm = document.querySelector(".popup__form");
-// Значения newUserName и newUserFunction вычисляются
-// в момент вызова submitPopupFormHandler
-let newUserName;
-let newUserFunction;
+let newUserName = popup.querySelector(".popup__field_value_name");
+let newUserProfession = popup.querySelector(".popup__field_value_profession");
 
 // PROFILE
 let editButton = document.querySelector(".profile__edit-button");
 let profileName = document.querySelector(".profile__name");
-let profileFunction = document.querySelector(".profile__profession");
+let profileProfession = document.querySelector(".profile__profession");
 
 function editProfileOpen() {
   popup.classList.add("popup_opened");
+  newUserName.value = profileName.innerText;
+  newUserProfession.value = profileProfession.innerText;
 }
 
 function editProfileClose() {
@@ -22,12 +22,8 @@ function editProfileClose() {
 
 function submitPopupFormHandler(evt) {
   evt.preventDefault();
-
-  newUserName = popup.querySelector(".popup__field_value_name").value;
-  newUserFunction = popup.querySelector(".popup__field_value_profession").value;
-
-  profileName.textContent = newUserName;
-  profileFunction.textContent = newUserFunction;
+  profileName.textContent = newUserName.value;
+  profileProfession.textContent = newUserProfession.value;
   editProfileClose();
 }
 
@@ -37,13 +33,13 @@ popupForm.addEventListener("submit", submitPopupFormHandler);
 
 // Эти переменная, слушатель и его хендлер likeToggler  будут использованы в следующем спринте
 
-let cards = document.querySelector(".cards");
+// let cards = document.querySelector(".cards");
 
-function likeToggler(event) {
-  target = event.target;
-  if (target.classList.contains("cards__like-button")) {
-    target.classList.toggle("cards__like-button_active");
-  }
-}
+// function likeToggler(event) {
+//   target = event.target;
+//   if (target.classList.contains("cards__like-button")) {
+//     target.classList.toggle("cards__like-button_active");
+//   }
+// }
 
-cards.addEventListener("click", likeToggler);
+// cards.addEventListener("click", likeToggler);
